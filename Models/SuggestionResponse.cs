8ff7a938df7c2a22eff723431926735dd0171c68
esmckay <70922535+esmckay@users.noreply.github.com>
2021-02-10 19:29:39 -0700

    yessir

--- Models/SuggestionResponse.cs
+++ Models/SuggestionResponse.cs
@@ -8,11 +8,12 @@ namespace Assignment4.Models
 {
     public class SuggestionResponse
     {
-        public string suggName { get; set; }
+        public string? suggName { get; set; } = "N/A";
+        [Required]
         public string suggRest { get; set; }
-        public string suggDish { get; set; }
+        public string? suggDish { get; set; } = "N/A";
 
-        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage ="Invalid Phone Number")]
-        public string suggPhone { get; set; }
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Invalid Phone Number")]
+        public string? suggPhone { get; set; } = "N/A";
     }
 }
